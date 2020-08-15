85e8e672f819ebd9211cc59d567a6a0653da40d6
Gaiokane <qk632773809@vip.qq.com>
2020-08-15 23:52:49 +0800

    优化：优化代码，调整导出方法、增加相应注释

--- WeChat_Favorites_Automatic_Category.py
+++ WeChat_Favorites_Automatic_Category.py
@@ -9,6 +9,17 @@ from operator import itemgetter
 # 该行修改favorites.db文件所在路径，确保db文件已禁用加密
 conn = sqlite3.connect('D:/share/WeChat DB/favorites.db')
 
+"""
+设置导出类型
+TYPE = 1：打印不排序的dict（含数量）
+TYPE = 2：打印降序的list（含数量）
+TYPE = 4：打印降序前三名（不显示数量）
+TYPE = 8：打印不排序dict，一行一个（含数量）
+TYPE = 16：打印降序dict，一行一个（含数量）
+TYPE = 3/5/7...可做与运算，打印多个
+ """
+TYPE = 16
+
 c = conn.cursor()
 
 # 默认按收藏时间降序，可自行调整
@@ -20,9 +31,10 @@ result = c.fetchall()
 # 截取字符串
 def xml_substring(start_str, end_str, str):
     rex = r'%s(.*?)%s' % (start_str, end_str)
-    #返回值list转string
+    # 返回值list转string
     return ''.join(re.findall(rex, str))
 
+# 存放取出来的所有关键字
 all_Keywords = []
 
 for xml in result:
@@ -30,31 +42,20 @@ for xml in result:
     xml_str = ''.join(xml)
     # 取出pagetitle、link并进行反转义
     pagetitle = html.unescape(xml_substring('<pagetitle>', '</pagetitle>', xml_str))
-    #关键词提取，循环中的元组合并
+    # 关键词提取，循环中的元组合并
     all_Keywords += HanLP.extractKeyword(pagetitle, 100)
 
-#打印合并后的元组
-#print(all_Keywords)
-
-#打印元组中出现最多次数的元素
-#print(max(all_Keywords, key=all_Keywords.count))
-
-# 存放结果
-out = ''
-
-#统计元组中元素出现的次数并排序显示
-def count_sort(strs,type):
+# 统计元组中元素出现的次数并排序显示
+def count_sort(strs, type):
     
+    # 存放结果
     out = ''
 
-    #1.统计各元素出现的次数，一行显示一个
-    """ dic = Counter(strs)
-    for i in dic.items():
-        print(i[0], i[1]) """
+    # 配合“打印不排序dict，一行一个（含数量）”使用
+    dic = Counter(strs)
     
-    #2.字典形式统计各元素出现的次数，不排序/降序
     count = 1
-    l = len(strs)-1
+    l = len(strs) - 1
     x = {}
     t = 0
     
@@ -71,49 +72,48 @@ def count_sort(strs,type):
         x[th] = count
         count = 1
         t += 1
-    #不排序
-    #print(x)
+
+    # 1.打印不排序的dict（含数量）
     if type & 1 != 0:
-        #global out
         out += str(x) + '\n'
     
-    if type == 3 or type == 5 or type == 7:
+    if type != 1 and type & 1 != 0:
         out += '——————————————————————————————————————————' + '\n'
 
-    #降序
+    # 2.打印降序的list（含数量）
     y = sorted(x.items(), key=lambda x: x[1], reverse=True)
-    #print(y)
-    
-    #显示出现次数最多的前三名
-    """ three = (y[0], y[1], y[2])
-    for t in three:
-        print(t[0]) """
     if type & 2 != 0:
-        three = (y[0], y[1], y[2])
-        for t in three:
-            #global out
-            out += t[0] + '\n'
+        out += str(y) + '\n'
     
-    if type == 7:
+    if type != 2 and type & 2 != 0:
         out += '——————————————————————————————————————————' + '\n'
     
-    #降序一行一个 输出
-    """ for k,v in y:
-        #print(k,v)
-        global out
-        out += k + ' ' + str(v) + '\n' """
+    # 3.打印降序前三名（不显示数量）
     if type & 4 != 0:
-        for k,v in y:
-            #global out
+        three = (y[0], y[1], y[2])
+        for t in three:
+            out += t[0] + '\n'
+    
+    if type != 4 and type & 4 != 0:
+        out += '——————————————————————————————————————————' + '\n'
+    
+    # 4.打印不排序dict，一行一个（含数量）
+    if type & 8 != 0:
+        for i in dic.items():
+            out += str(i[0]) + ' ' + str(i[1]) + '\n'
+    
+    if type != 8 and type & 8 != 0:
+        out += '——————————————————————————————————————————' + '\n'
+
+    # 5.打印降序dict，一行一个（含数量）
+    if type & 16 != 0:
+        for k, v in y:
             out += k + ' ' + str(v) + '\n'
 
     return out
 
-#count_sort(all_Keywords,7)
-#print(count_sort(all_Keywords,3))
-
 # 输出文件
-with open('WeChat_Favorites_Automatic_Category.txt', 'w+', encoding='utf-8') as f:
-    f.write(count_sort(all_Keywords,7))
+with open('WeChat_Favorites_Automatic_Category.txt', 'w+', encoding = 'utf-8') as f:
+    f.write(count_sort(all_Keywords, TYPE))
 
 print('导出成功')
